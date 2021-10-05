dbf7c00db901e56232ddbe1394a6942858ba4067
PatxiEH <patxi.hiribarren@gmail.com>
2021-10-05 22:45:32 +0200

    Add js open modal and change bacground color

--- portfolio.js
+++ portfolio.js
@@ -4,26 +4,30 @@
 const modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-const img = document.getElementById("img2");
-const modalImg = document.getElementById("modalContent");
-
+const img = document.getElementsByClassName("imagegallery");
+const modalImg = document.getElementById("myModal");
+const modalContent = document.getElementById("modalContent");
+const modalCaption = document.getElementById("caption")
 
 // On click, open modal and display image
-img.addEventListener('click' , () =>  {
+for (let i = 0; i < img.length; i++ ) {
+  img[i].addEventListener('click',() => {
+  
+  //modalImg.src = img.src;
+  modalImg.style.display = "block";
+  modalContent.src = img[i].src;
+  console.log (img[i].src);
+  modalCaption.innerHTML = img[i].alt;
+  });
+}
 
-  
-  modal.style.display = "block";
-  modalImg.src = img.src;
 
-});
-  
-  
+
 // Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
+//When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
-
 
