2e26780c97245c5fea654cfca093c328f7940437
ValentinLagues <valentin.lagues@gmail.com>
2021-10-05 12:28:08 +0200

    branch contact created

---
+++ contact.js
@@ -0,0 +1,5 @@
+let myFormButton = document.getElementById('formButton');
+myFormButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    alert("Votre demande a bien été prise en compte, vous recevrez une réponse dans les plus brefs délais.")
+})
