171fb0222535aabf66ecca230c2ee3ac0d4beaf4
PatxiEH <patxi.hiribarren@gmail.com>
2021-10-07 21:07:53 +0200

    Add code comments

--- portfolio.js
+++ portfolio.js
@@ -3,17 +3,18 @@
 // Get the modal
 const modal = document.getElementById("myModal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
+// Get the image and insert it inside the modal 
 const img = document.getElementsByClassName("imagegallery");
 const modalImg = document.getElementById("myModal");
 const modalContent = document.getElementById("modalContent");
 const modalCaption = document.getElementById("caption")
 
 // On click, open modal and display image
+// Run classe img with for loop
+// Use its "alt" text as a caption
 for (let i = 0; i < img.length; i++ ) {
   img[i].addEventListener('click',() => {
   
-  //modalImg.src = img.src;
   modalImg.style.display = "block";
   modalContent.src = img[i].src;
   console.log (img[i].src);
